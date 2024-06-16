684d9a69592ad3c45066f7e7018d6f6cc04d22a4
dwrik <wrikdas22@gmail.com>
2024-06-16 15:56:14 +0530

    implemented api gateway

---
+++ gateway/src/main/java/org/dwrik/gateway/config/RouteConfiguration.java
@@ -0,0 +1,22 @@
+package org.dwrik.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+
+    @Bean
+    public RouteLocator buildRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("eureka-dashboard", r -> r.path("/eureka")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://eureka"))
+                .route("eureka", r -> r.path("/eureka/**")
+                        .uri("lb://eureka")
+                ).build();
+    }
+
+}
