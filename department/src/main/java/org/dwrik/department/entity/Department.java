ac0f102dbcf4ae7e06f018c56f8ce34c0a1ad94b
Wrik Das <wrikdas22@gmail.com>
2024-06-19 16:56:07 +0530

    added constructor annotations

--- department/src/main/java/org/dwrik/department/entity/Department.java
+++ department/src/main/java/org/dwrik/department/entity/Department.java
@@ -2,10 +2,14 @@ package org.dwrik.department.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(indexes = @Index(name = "dept_name_index", columnList = "name", unique = true))
 public class Department {
 
