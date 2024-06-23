3b3365191d31939286761e877f5d598d72e511e2
dwrik <wrikdas22@gmail.com>
2024-06-23 13:35:08 +0530

    preload data for easier dev workflow

--- department/src/main/java/org/dwrik/department/DepartmentApplication.java
+++ department/src/main/java/org/dwrik/department/DepartmentApplication.java
@@ -1,13 +1,35 @@
 package org.dwrik.department;
 
+import org.dwrik.department.entity.Department;
+import org.dwrik.department.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DepartmentApplication {
+import java.util.List;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DepartmentApplication.class, args);
-	}
+@SpringBootApplication
+public class DepartmentApplication implements CommandLineRunner {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // pre-populating the database
+        departmentRepository.saveAll(List.of(
+                new Department(1L, "Sales"),
+                new Department(2L, "Accounting"),
+                new Department(3L, "Quality Assurance"),
+                new Department(4L, "Customer Relations"),
+                new Department(5L, "Customer Service"),
+                new Department(6L, "HR")
+        ));
+    }
 
 }
