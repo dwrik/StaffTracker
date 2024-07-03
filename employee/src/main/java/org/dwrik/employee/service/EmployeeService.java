1c3dad87b4f2862088e456b37396f1f45ae76121
dwrik <wrikdas22@gmail.com>
2024-07-03 21:45:17 +0530

    added service level logging for external REST call

--- employee/src/main/java/org/dwrik/employee/service/EmployeeService.java
+++ employee/src/main/java/org/dwrik/employee/service/EmployeeService.java
@@ -1,14 +1,20 @@
 package org.dwrik.employee.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.dwrik.employee.entity.Employee;
 import org.dwrik.employee.exception.InvalidDepartmentException;
 import org.dwrik.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 
+import java.io.IOException;
+
+@Slf4j
 @Service
 public class EmployeeService {
 
@@ -24,16 +30,21 @@ public class EmployeeService {
     @Transactional
     public Employee createEmployee(Employee employee) {
         Long departmentId = employee.getDepartmentId();
+        long startTime = System.currentTimeMillis();
 
         restClient.get()
                 .uri("lb://department/{departmentId}", departmentId)
                 .retrieve()
                 .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                    long duration = System.currentTimeMillis() - startTime;
+                    logAPIRequest(request, response, duration, "Server error when connecting with department service, skipping creation...");
                     throw new InvalidDepartmentException(
-                            String.format("unable to confirm existence of department with id '%d', please try again later", departmentId)
+                            String.format("unable to confirm existence of department with id '%d', try again later", departmentId)
                     );
                 })
                 .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    long duration = System.currentTimeMillis() - startTime;
+                    logAPIRequest(request, response, duration, "Unable to confirm existence of department with id '" + departmentId + "', skipping creation...");
                     throw new InvalidDepartmentException(
                             String.format("department with id '%d' does not exist", departmentId)
                     );
@@ -67,6 +78,22 @@ public class EmployeeService {
     @Transactional
     public void deleteEmployeesByDepartmentId(Long departmentId) {
         employeeRepository.deleteByDepartmentId(departmentId);
+    }
+
+    private void logAPIRequest(HttpRequest request, ClientHttpResponse response, long duration, String message) throws IOException {
+        var path = "/" + request.getURI().getHost() + request.getURI().getPath();
+        log.info("{} {} {} {} {} {}",
+                fixedWidth(request.getMethod(), 6),
+                fixedWidth(path, 20),
+                fixedWidth("HTTP/1.1", 8),
+                fixedWidth(response.getStatusCode().value(), 3),
+                fixedWidth(duration + "ms", 6),
+                message
+        );
+    }
+
+    private static String fixedWidth(Object object, int width) {
+        return String.format("%-" + width + "s", object);
     }
 
 }
